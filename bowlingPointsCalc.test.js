2ab468aefefbd4a5e64253fd9ee3aed89ec93e6c
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2023-01-18 12:51:36 +0530

    chore: tests for best score function

--- bowlingPointsCalc.test.js
+++ bowlingPointsCalc.test.js
@@ -3,11 +3,20 @@ const { score, bestScore } = require('./bowlingPointsCalc.js')
 describe('Bowling Game Calculator', () => {
     describe('score', () => {
         it('should return no game played when array is empty', () => {
-            expect(() => score([])).toBe('no game played');
+            let result = score([])
+            expect(result).toEqual('no game played');
         })
 
         it('should give score when array is valid', () => {
             let result = score([6,3]);
+            expect(result).toBe(9);
+        })
+    })
+
+    describe('bestscore', () => {
+
+        it('should give max score when array is valid', () => {
+            let result = bestScore([[6,3],[6,2]]);
             expect(result).toBe(9);
         })
     })
